3cf268539a8fc9a1b0ad20799fc8019dcb08788e
Sergei-Korneev <60899505+Sergei-Korneev@users.noreply.github.com>
2020-03-21 00:07:20 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    var nn="";
+    var str=n.toString(10).replace("-","");
+    var ln=str.length;
+    
+    for (i=ln-1; i>=0; i--){
+        nn += str[i];
+
+    }
+    return parseInt(nn,10);
 }
